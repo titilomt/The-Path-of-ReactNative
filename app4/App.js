edbad5ae5356c11746557671c66c79e68cf91b7e
T.Fonseca <tiaguinho1337@gmail.com>
2018-12-26 19:15:53 -0200

    Minor fix

--- app4/App.js
+++ app4/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ListaItens from './src/components/ListaItens';
 //import { StyleSheet, Text, View } from 'react-native';
 
-
 export default class App extends React.Component {
   render() {
     return (
